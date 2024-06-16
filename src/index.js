6d7eb436c1be693926f303a8990cf30c435ecd20
Serhii-Kuzmin <seryy8248@gmail.com>
2024-06-16 19:00:15 +0200

    transform data and component of random person

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
 import "./style/style.scss";
 
-import MarvelService from "./services/MarvelServices";
+// import MarvelService from "./services/MarvelServices";
 
-const marvelService = new MarvelService();
 // marvelService.getAllCharacters().then((res) => console.log(res));
-
-marvelService.getAllCharacters().then((res) =>
-  res.data.results.forEach((item) => {
-    console.log(item.name);
-  })
-);
 
 // marvelService.getCharacter(1011052).then((res) => console.log(res));
 
