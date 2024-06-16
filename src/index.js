e425f710d5af01a1c82d47f88e7cbda7c623c4c5
Serhii-Kuzmin <seryy8248@gmail.com>
2024-06-16 17:44:09 +0200

    fetch-url

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
 import "./style/style.scss";
 
+import MarvelService from "./services/MarvelServices";
+
+const marvelService = new MarvelService();
+// marvelService.getAllCharacters().then((res) => console.log(res));
+
+marvelService.getAllCharacters().then((res) =>
+  res.data.results.forEach((item) => {
+    console.log(item.name);
+  })
+);
+
+// marvelService.getCharacter(1011052).then((res) => console.log(res));
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
