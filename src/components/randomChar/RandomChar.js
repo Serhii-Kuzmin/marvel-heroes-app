bcda154195bd527d38fe11a55f9090ce09969df6
Serhii-Kuzmin <seryy8248@gmail.com>
2024-06-17 18:54:28 +0200

    random-char-click-on-button

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -38,8 +38,15 @@ class RandomChar extends Component {
     this.setState({ char, loading: false });
   };
 
+  onCharLoading = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onChatLoaded)
@@ -64,7 +71,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button onClick={this.updateChar} className="button button__main">
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
